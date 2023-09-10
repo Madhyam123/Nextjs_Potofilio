806ec4c9d99cf147c325f8d98e43b1e2fb7e79b4
madhyam bharati <96839514+Madhyam123@users.noreply.github.com>
2023-09-10 14:47:16 +0530

    Update index.jsx

--- src/components/Projects/components/project/index.jsx
+++ src/components/Projects/components/project/index.jsx
@@ -2,12 +2,24 @@
 import React from 'react'
 import styles from './style.module.scss';
 
-export default function index({index, title, manageModal}) {
+export default function index({index, title,stack,Feature, manageModal}) {
 
     return (
         <div onMouseEnter={(e) => {manageModal(true, index, e.clientX, e.clientY)}} onMouseLeave={(e) => {manageModal(false, index, e.clientX, e.clientY)}} className={styles.project}>
             <h2>{title}</h2>
-            <p>Design & Development</p>
+            <div>
+            <h3>{stack}</h3>
+            <h5>{Feature}</h5>
+            </div>
+            
+            {/* <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5>
+            <h5>.MONGODB</h5> */}
         </div>
     )
 }
